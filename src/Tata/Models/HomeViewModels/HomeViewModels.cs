e931d30988eefc5c4c74ac203ad0b58e4e9e9a15
Squall Taki <taki.lnt@gmail.com>
2017-02-05 00:03:02 +0700

    - Add service controller (aka product) and its pages. - Add initial data seed.

---
+++ src/Tata/Models/HomeViewModels/HomeViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Tata.Entities;
+
+namespace Tata.Models
+{
+    public class HomeViewModels
+    {
+        public IEnumerable<Setting> CommonSettings { get; set; }
+    }
+}
