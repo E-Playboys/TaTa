5b23020edc44850c578045562953c79e0c2d6d6e
Tin Ngoc le <tin.ngoc.le@steadfasttech.com.au>
2017-02-08 14:36:13 +0700

    - Update setting table. - Bind data to footer.

--- src/Tata/Startup.cs
+++ src/Tata/Startup.cs
@@ -116,7 +116,7 @@ namespace Tata
             public ApplicationDbContext Create(DbContextFactoryOptions options)
             {
                 var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-                builder.UseSqlServer("Data Source=.;Initial Catalog=TaTa;Integrated Security=True");
+                builder.UseSqlServer("Data Source=.\\MSSQLSERVER2012;Initial Catalog=TaTa;Integrated Security=True");
                 return new ApplicationDbContext(builder.Options);
             }
         }
