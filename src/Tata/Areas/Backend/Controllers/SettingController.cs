f1e0edb5aa8056304c0690d4bac663fdb4daf54a
Tin Ngoc le <tin.ngoc.le@steadfasttech.com.au>
2017-03-06 17:35:25 +0700

    - Finish setting page.

--- src/Tata/Areas/Backend/Controllers/SettingController.cs
+++ src/Tata/Areas/Backend/Controllers/SettingController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tata.Areas.Backend.Models.Product;
 using Tata.Areas.Backend.Models.Setting;
 using Tata.Entities;
 using TaTa.DataAccess;
@@ -32,11 +33,29 @@ namespace Tata.Areas.Backend.Controllers
             using (IUnitOfWork uow = _uowProvider.CreateUnitOfWork())
             {
                 var productRepo = uow.GetRepository<Setting>();
-                IEnumerable<Setting> settings = (await productRepo.GetAllAsync()).Skip(skip).Take(take).OrderBy(s => s.Id);
+                IEnumerable<Setting> settings = (await productRepo.GetAllAsync()).OrderBy(s => s.Id);
                 IEnumerable<SettingModel> models = Mapper.Instance.Map<IEnumerable<Setting>,IEnumerable<SettingModel>>(settings);
 
                 return View(models);
             }
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            using (IUnitOfWork uow = _uowProvider.CreateUnitOfWork())
+            {
+                var productRepo = uow.GetRepository<Setting>();
+                Setting setting = await productRepo.GetAsync(id);
+                SettingModel models = Mapper.Instance.Map<Setting, SettingModel>(setting);
+
+                return View(models);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Details(SettingModel model)
+        {
+            return View();
         }
     }
 }
