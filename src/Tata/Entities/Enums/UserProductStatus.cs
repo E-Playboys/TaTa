15feca9dfe5415e3da5490f5110b66b0c69df453
Khang Tran <mr.khangtran@gmail.com>
2017-03-25 23:28:45 +0700

    add user product

---
+++ src/Tata/Entities/Enums/UserProductStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tata.Entities.Enums
+{
+    public enum UserProductStatus
+    {
+        [Display(Name = "Not Process")]
+        NotProcess,
+
+        [Display(Name = "Processed")]
+        Processing,
+
+        [Display(Name = "Done")]
+        Done
+    }
+}
